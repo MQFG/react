988cb9d7792b96e916b905b531a4a11f58ee3849
MQFG <106691851+MQFG@users.noreply.github.com>
2023-08-15 21:43:32 +0000

    to add images

---
+++ 03-pizza-menu/public/data.js
@@ -0,0 +1,16 @@
+const pizzaData = [
+    {
+        name: "Focacia",
+        ingredients: "Bread, Cheese, Tomato",
+        price: 6,
+        photoName: "pizzas/focaccia.jpg",
+        soldOut: false
+    },
+    {
+        name: "Pizza Magrherita",
+        ingredients: "Tomato and mozzarella",
+        price: 10,
+        photoName: "pizzas/focaccia.jpg",
+        soldOut: false
+    }
+]
